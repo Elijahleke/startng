e9a5934c95f02958fdb1d5f9fd002ea7fb934a5a
elijaleke <adeleked@gmail.com>
2019-09-08 03:02:06 +0100

    get form

--- getform.php
+++ getform.php
@@ -1,10 +1,14 @@
 <?php
-echo $_POST["name"];
-echo "<br><br>";
-echo $_POST["email"];
-echo "<br><br>";
-echo $_POST["title"];
-echo "<br><br>";
-echo "<br><br>";
-echo $_POST["message"];
+if ( isset( $_POST['submit'] ) ) {
+$fname=$_POST['fname'];
+$email=$_POST['email'];
+$title=$_POST['title'];
+$message=$_POST['message'];
+
+echo "Your name is " . $fname ."<br>" ; 
+echo "Sender email is " . $email ."<br>" ; 
+echo "Title of the message is " . $title ."<br>" ; 
+echo  . $message ."<br>" ; 
+
+}
 ?>
